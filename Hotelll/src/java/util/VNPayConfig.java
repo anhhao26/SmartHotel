2743fa8288503fa8b0f973b8bcf2a7ed913d414d
BailSharrd <ntphongusa@gmail.com>
2026-03-22 22:57:17 +0700

    vnpay

--- Hotelll/src/java/util/VNPayConfig.java
+++ Hotelll/src/java/util/VNPayConfig.java
@@ -1,13 +1,13 @@
 package util;
 
 public class VNPayConfig {
+    // Thông tin Sandbox mới nhất từ email VNPAY (09C794F4)
     public static String vnp_TmnCode = "09C794F4";
     
-    // Sử dụng mã HashSecret mới nhất từ hình Screenshot 2026-03-04 003004.png
     public static String vnp_HashSecret = "VPP4CETHK6KY1D4CVZF4VQ2OLAQVEXRT";
     
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
 
-    // URL KOYEB (Đã deploy - Không cần ngrok nữa)
-    public static String vnp_ReturnUrl= "https://damp-beatrisa-tyberos-99b62712.koyeb.app/vnpay_return";
+    // URL callback về web Koyeb sau khi thanh toán xong
+    public static String vnp_ReturnUrl = "https://damp-beatrisa-tyberos-99b62712.koyeb.app/vnpay_return";
 }
