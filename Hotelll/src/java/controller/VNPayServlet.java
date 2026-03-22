658cf0e67c16adc9e697bea6408243e06ba2a1bf
BailSharrd <ntphongusa@gmail.com>
2026-03-22 22:42:14 +0700

    Update VNPayServlet.java

--- Hotelll/src/java/controller/VNPayServlet.java
+++ Hotelll/src/java/controller/VNPayServlet.java
@@ -56,7 +56,7 @@ public class VNPayServlet extends HttpServlet {
         String vnp_OrderInfo = "Thanh toan don dat phong " + bookingId;
         String vnp_IpAddr = request.getRemoteAddr();
         
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         
